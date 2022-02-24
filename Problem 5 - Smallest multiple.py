cb59a0812d4289e5c711c58c9025ddf0630f13f1
krushang patel <36439385+kushpatel544@users.noreply.github.com>
2022-02-24 12:52:54 -0600

    Problem 5

--- Problem 5 - Smallest multiple.py
+++ Problem 5 - Smallest multiple.py
@@ -2,25 +2,24 @@
 
 import math
 #  using brute force
-def isDiv(n):
-    for x in range(1,21):
-        if(n%x!=0):
-            return False
-    return True
+# def isDiv(n):
+#     for x in range(1,21):
+#         if(n%x!=0):
+#             return False
+#     return True
 
-n=2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20
-for x in range(100,n,10):
-    print(x)
-    if(isDiv(x)):
-        print(x)
-        break
-    x-=1
+# n=2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20
+# for x in range(100,n,10):
+#     print(x)
+#     if(isDiv(x)):
+#         print(x)
+#         break
+#     x-=1
 
 
-"""
+
 #using LCM(a,b)=a*b/gcd(a,b)
 s=1
 for x in range(1,21):
     s=s*x//math.gcd(x,s)
 print(str(s))
-"""
